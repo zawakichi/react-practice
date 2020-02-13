a470e47288af7c31fb314daca182f5a444d3833e
zawakichi_ <zawakichi21@gmail.com>
2020-02-13 23:56:37 +0900

    init

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # react-practice
+
+ npm init -y
+ npm install -D webpack webpack-cli babel-loader @babel/core  @babel/preset-env @babel/preset-react
+ npm install react react-dom
+ npm install -D eslint
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+module.exports = {
+  // モード値を production に設定すると最適化された状態で、
+  // development に設定するとソースマップ有効でJSファイルが出力される
+  mode: "production",
+  entry: './src/main.js',
+  // ファイルの出力設定
+  output: {
+    //  出力ファイルのディレクトリ名
+    path: `${__dirname}/dist`,
+    // 出力ファイル名
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        // 拡張子 .js の場合
+        test: /\.js$/,
+        use: [
+          {
+            // Babel を利用する
+            loader: "babel-loader",
+            // Babel のオプションを指定する
+            options: {
+              presets: [
+                // プリセットを指定することで、ES2020 を ES5 に変換
+                "@babel/preset-env",
+                // React の JSX を解釈
+                "@babel/react"
+              ]
+            }
+          }
+        ]
+      },
+      {
+        // 拡張子 .js の場合
+        test: /\.jsx$/,
+        use: [
+          {
+            // Babel を利用する
+            loader: "babel-loader",
+            // Babel のオプションを指定する
+            options: {
+              presets: [
+                // プリセットを指定することで、ES2020 を ES5 に変換
+                "@babel/preset-env",
+                // React の JSX を解釈
+                "@babel/react"
+              ]
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
